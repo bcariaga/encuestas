7d5dbd36e4f8e72a5b8d3d7fdcc9757552eb71cc
bcariaga <cariaga.bh@gmail.com>
2018-11-11 22:09:14 -0300

    agrego funciones para la clase sujeto (parcial)

---
+++ codigo/sujeto.cpp
@@ -0,0 +1,22 @@
+/*
+Archivo: sujeto.cpp
+
+Propósito:  funciones de sujeto.h
+
+*/
+
+#include <iostream>
+#include "headers/configuracion.h" //para fijar el tamaño de los arrays
+#include "headers/sujeto.h" // para la propiedad sujetosEncuestados
+#include "headers/encuesta.h"
+
+using namespace std; 
+
+void Sujeto::responder(Pregunta[ENCUESTA_CANTIDAD_PREGUNTAS] preguntas)
+{
+    
+}
+string Sujeto::getRespuesta(int indice)
+{
+    
+}
