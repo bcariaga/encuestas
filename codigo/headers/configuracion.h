8b87de8a4c036ee4e9a76378605c0df1baa721d1
bcariaga <cariaga.bh@gmail.com>
2018-11-11 22:06:53 -0300

    agrego clave para salir de las preguntas

--- codigo/headers/configuracion.h
+++ codigo/headers/configuracion.h
@@ -7,10 +7,14 @@ Propósito:  Centralizar las configuraciones del programa y
 #ifndef CONFIGURACION
 #define CONFIGURACION
 
+#include <iostream>
+
 
 const int ENCUESTA_CANTIDAD_MAXIMA_SUJETOS = 500; //para delimitar la cantidad de sujetos a encuestar (Encuesta::sujetosEncuestados)
 const int ENCUESTA_CANTIDAD_PREGUNTAS = 2; //para delimitar la cantidad de preguntas a realizar (Encuesta::preguntas)
 
 const int PREGUNTA_CANTIDAD_MAXIMA_OPCIONES = 10; //para delimitar la cantidad de preguntas a realizar (Encuesta::preguntas)
+
+const std::string PREGUNTA_PALABRA_ESCAPE = "nn";
 
 #endif
